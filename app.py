e80470f018de9a508723267e5d634a0545959215
marcosvandrade <vascorj2006@gmail.com>
2021-10-08 17:49:03 -0300

    Testando cotações de bitcoins

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ import plotly.graph_objs as go
 from streamlit_autorefresh import st_autorefresh
 
 
-countries = ['Argentina',
-             'Australia', 'Austria', 'Bangladesh',
-             'Belgium',
-             'Bosnia', 'Brazil', 'Bulgaria',
-             'Canada', 'China', 'Colombia', 'Croatia', 'Czech Republic', 'Denmark', 'Dubai', 'Egypt',
-             'Finland', 'France', 'Germany', 'Greece', 'Hong Kong', 'Hungary', 'Iceland', 'India',
-             'Indonesia', 'Ireland', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan',
-             'Kuwait', 'Lebanon', 'Malawi', 'Malaysia', 'Malta', 'Mauritius', 'Mexico', 'Mongolia',
-             'Montenegro', 'Morocco', 'Namibia', 'Netherlands', 'New Zealand', 'Nigeria',  'Norway',
-             'Oman', 'Pakistan', 'Palestine',  'Peru',  'Philippines', 'Poland', 'Portugal', 'Qatar',
-             'Romania', 'Russia', 'Rwanda',  'Saudi Arabia', 'Serbia', 'Singapore', 'Slovenia',
-             'South Africa', 'South Korea', 'Spain',  'Sri Lanka', 'Sweden', 'Switzerland', 'Taiwan',
-             'Tanzania', 'Tunisia', 'Ukraine', 'United Kingdom', 'United States', 'Venezuela',
+countries = ['Argentina', 'Australia', 'Austria', 'Bangladesh', 'Belgium',
+             'Bosnia', 'Brazil', 'Bulgaria', 'Canada', 'China', 'Colombia',
+             'Croatia', 'Czech Republic', 'Denmark', 'Dubai', 'Egypt',
+             'Finland', 'France', 'Germany', 'Greece', 'Hong Kong', 'Hungary',
+             'Iceland', 'India', 'Indonesia', 'Ireland', 'Italy',
+             'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan',
+             'Kuwait', 'Lebanon', 'Malawi', 'Malaysia', 'Malta', 'Mauritius',
+             'Mexico', 'Mongolia', 'Montenegro', 'Morocco', 'Namibia',
+             'Netherlands', 'New Zealand', 'Nigeria',  'Norway', 'Oman',
+             'Pakistan', 'Palestine', 'Peru',  'Philippines', 'Poland',
+             'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda',
+             'Saudi Arabia', 'Serbia', 'Singapore', 'Slovenia',
+             'South Africa', 'South Korea', 'Spain',  'Sri Lanka', 'Sweden',
+             'Switzerland', 'Taiwan', 'Tanzania', 'Tunisia', 'Ukraine',
+             'United Kingdom', 'United States', 'Venezuela',
              'Vietnam', 'Zambia']
 
 intervals = ['Daily', 'Weekly', 'Monthly']
@@ -31,6 +33,13 @@ def consultar_acao(stock, country, from_date, to_date, interval):
     df = ip.get_stock_historical_data(
         stock=stock, country=country, from_date=from_date,
         to_date=to_date, interval=interval)
+    return df
+
+
+@st.cache(allow_output_mutation=True)
+def consultar_crypto(crypto, from_date, to_date, interval):
+    df = ip.get_crypto_historical_data(
+        crypto=crypto, from_date=from_date, to_date=to_date, interval=interval)
     return df
 
 
@@ -61,7 +70,9 @@ def plotCandleStick(df, acao='ticket'):
 barra_lateral = st.sidebar.empty()
 country_select = st.sidebar.selectbox("Select country:", countries)
 stocks = ip.get_stocks_list(country=country_select)
+cryptos = ip.get_cryptos_list()
 stock_select = st.sidebar.selectbox("Select the stock:", stocks)
+crypto_select = st.sidebar.selectbox("Select the Cryptocurrency:", cryptos)
 from_date = st.sidebar.date_input('Start Date:', start_date)
 to_date = st.sidebar.date_input('End Date:', end_date)
 interval_select = st.sidebar.selectbox("Select the range:", intervals)
@@ -82,7 +93,10 @@ st.subheader('Graphical visualization')
 if from_date > to_date:
     st.sidebar.error('Data de ínicio maior do que data final')
 else:
-    df = consultar_acao(stock_select, country_select, format_date(
+    # df = consultar_acao(stock_select, country_select, format_date(
+    #     from_date), format_date(to_date), interval_select)
+
+    df = consultar_crypto(crypto_select, format_date(
         from_date), format_date(to_date), interval_select)
     try:
         fig = plotCandleStick(df)
